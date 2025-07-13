351ebd50de5e43b4232b15dc53eb64ec22fb35bc
Anthony-YV0908 <anthonyyuvega09@gmail.com>
2025-07-13 16:47:26 +0800

    module setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { ButtonModule } from 'primeng/button';
+import { SharedModule } from './shared/components/shared/shared.module';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ButtonModule],
+  imports: [RouterOutlet,ButtonModule,SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
@@ -10,6 +10,28 @@ export class ButtonComponent {
   @Input() label = 'Click Me';
   @Input() type: 'button' | 'submit' = 'button';
   @Input() color : 'primary' | 'success' | 'danger' | 'outline' = 'primary';
-  
+  @Input() icon?: string;
+  @Input() loading = false;
+  @Input() disabled = false;
+  @Input() fullWidth = false;
+  @Input() userPrime = false;
 
+
+  @Output() clicked = new EventEmitter<void>();
+
+  get buttonClass():string{
+
+    const base = `inline-flex items-center justify-center gap-2 px-4 py-2 text-sm font-medium rounded transition-all duration-150`
+
+    const colorMap:Record<string, string> = {
+      primary: 'bg-blue-600 text-white hover:bg-blue-700 focus:ring-blue-500',
+      success: 'bg-green-600 text-white hover:bg-green-700 focus:ring-green-500',
+      danger: 'bg-red-600 text-white hover:bg-red-700 focus:ring-red-500',
+      outline: 'border border-gray-300 text-gray-700 hover:bg-gray-100 focus:ring-gray-500'
+    }
+
+
+
+    return `${base } ${colorMap[this.color]}`;
+  }
 }
