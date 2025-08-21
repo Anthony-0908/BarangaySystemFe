5a5e4b2a22fce652fa4cc77c03476ea79f1f2cab
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-08-21 16:26:51 +0800

    sidenavbar

---
+++ src/app/layouts/sidenavbar/sidenavbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenavbar',
+  imports: [],
+  templateUrl: './sidenavbar.component.html',
+  styleUrl: './sidenavbar.component.css'
+})
+export class SidenavbarComponent {
+
+}
