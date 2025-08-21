a7d33df6ba6a2298cf8a8379db9d8fe704afc83a
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-08-21 16:26:01 +0800

    layouting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { MasterLayoutComponent } from './layouts/master-layout/master-layout.component';
 
+import { DashboardComponent } from './Admin/dashboard/dashboard.component';
 export const routes: Routes = [
   {
     path:'',
@@ -8,11 +10,25 @@ export const routes: Routes = [
     pathMatch: 'full'
   },
 
+
+
   {
     path:'login',
     loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) ,
     title: 'Login'
   },
+
+  {
+    path:'', 
+    component:MasterLayoutComponent,
+    children:[
+      {
+        path:'dashboard',
+        loadComponent: () => import('./Admin/dashboard/dashboard.component').then(m => m.DashboardComponent),
+        title: 'Dashboard'
+      }
+    ]
+  }
   // {
   //   path:'dashboard',
   //   loadComponent:() => import('./')
