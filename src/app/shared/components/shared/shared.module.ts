a037bfb71fe55039ca4ed63df31a30e446118ee7
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-08-27 00:12:19 +0800

    added table component to import  and export

--- src/app/shared/components/shared/shared.module.ts
+++ src/app/shared/components/shared/shared.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
 import { InputComponent } from '../input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { TableComponent } from '../table/table.component';
 
 
 @NgModule({
@@ -12,11 +12,13 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     ReactiveFormsModule,
     InputComponent,   // ✅ Standalone component must be imported
-    ButtonComponent   // ✅ Also standalone
+    ButtonComponent,   // ✅ Also standalone\
+    TableComponent,
   ],
   exports: [
     InputComponent,   // ✅ Export it AFTER it's imported
-    ButtonComponent
+    ButtonComponent,
+    TableComponent,
   ]
 })
 export class SharedModule {}
