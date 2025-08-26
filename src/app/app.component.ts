6d7675206b6122a2608c49ed35b89e4097a1aa6e
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-08-27 00:11:06 +0800

    added http cllientmodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { SharedModule } from './shared/components/shared/shared.module';
@@ -8,7 +9,11 @@ import { SharedModule } from './shared/components/shared/shared.module';
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet,SharedModule,ReactiveFormsModule],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    ReactiveFormsModule,
+    SharedModule,],
   template: `<router-outlet></router-outlet>`,
   styleUrl: './app.component.css',
 
