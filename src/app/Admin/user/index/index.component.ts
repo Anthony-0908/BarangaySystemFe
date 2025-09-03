b505a9aeec194baec8437a777ec2f74871820d60
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-03 17:27:39 +0800

    added a store inject

--- src/app/Admin/user/index/index.component.ts
+++ src/app/Admin/user/index/index.component.ts
@@ -1,38 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from '../../../shared/components/table/table.component';
 import { UserService } from '../../../core/service/user.service';
 import { User } from '../../../model/user';
+import { IndexStore } from './index.store';
 @Component({
   selector: 'app-index',
-  imports: [CommonModule,TableComponent],
+  imports: [CommonModule],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.css'
+
 })
 export class IndexComponent implements OnInit {
 
-  users: User[] = [];
+  store = inject(IndexStore)
 
-columns = [
-    { field: 'id', header: 'ID' },
-    { field: 'name', header: 'Name' },
-    { field: 'email', header: 'Email' },
-    {field:'actions', header:'Actions'}
-];
+// columns = [
+//     { field: 'id', header: 'ID' },
+//     { field: 'name', header: 'Name' },
+//     { field: 'email', header: 'Email' },
+//     {field:'actions', header:'Actions'}
+// ];
 
- constructor(private userService: UserService) {}
-  ngOnInit() {
-     this.loadUsers();
+//  constructor(private userService: UserService) {}
+  ngOnInit():void {
+     this.store.loadUsers();
   }
 
-  loadUsers(): void 
-  {
-    this.userService.getUsers().subscribe({
-      next:(data) => {
-        console.log('users fetched' , data);
-        this.users = data
-      },
-      error:(err) => console.error('Error fetching users', err)
-    })
-  }
+  // loadUsers(): void 
+  // {
+  //   this.userService.getUsers().subscribe({
+  //     next:(data) => {
+  //       console.log('users fetched' , data);
+  //       this.users = data
+  //     },
+  //     error:(err) => console.error('Error fetching users', err)
+  //   })
+  // }
 }
