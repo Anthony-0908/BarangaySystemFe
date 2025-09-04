5ae28f88f8c653674ade4b3d2b17e0ba32400034
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-04 15:04:24 +0800

    User Routes

--- src/app/Admin/user/user.route.ts
+++ src/app/Admin/user/user.route.ts
@@ -1,8 +1,13 @@
 import {Routes} from "@angular/router";
 import { IndexComponent } from "./index/index.component";
+import { CreateComponent } from "./create/create.component";
 export const USERS_ROUTES: Routes = [ 
   {
-    path:'',
-    loadComponent:() => import("./index/index.component").then(m => m.IndexComponent)
+    path:'index',
+    loadComponent:() => import("./index/index.component").then(m => m.IndexComponent),
+  },
+  {
+    path:'create',
+    loadComponent:() => import('./create/create.component').then(m => m.CreateComponent)
   }
-]
+]     
