fb1cc326c613305c537c41cde0e340975509a3e2
Anthony-YV0908 <anthony.yuvega@radenta.com>
2025-09-13 18:06:43 +0800

    reset-password services

---
+++ src/app/core/service/reset-password.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable} from "rxjs";
+import { API_URL } from '../constants/api.constants';
+import { User } from '../../model/user';
+
+@Injectable({
+  providedIn: 'root' // makes the service available app-wide
+})
+
+export class AuthService { 
+  private apiurl = `${API_URL}/auth`;
+
+  constructor(private http: HttpClient){}
+
+  resetpassword(user: string): Observable<any>{
+    return this.http.post<any>(`${this.apiurl}/reset-password`,user);
+  }
+}
