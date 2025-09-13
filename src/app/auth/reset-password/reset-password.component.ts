3a1321afdd6583563b1fe565e185f89eb8084432
Anthony-YV0908 <anthony.yuvega@radenta.com>
2025-09-13 18:08:46 +0800

    reset-password component

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { InputComponent } from "../../shared/components/input/input.component";
+import { ButtonComponent } from "../../shared/components/button/button.component";
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule  } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [],
+  imports: [InputComponent, ButtonComponent,ReactiveFormsModule],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.css'
 })
 export class ResetPasswordComponent {
 
+   protected readonly resetPasswordForm!: FormGroup;
+  loading = false;
+
+  constructor(private fb: FormBuilder) {
+    this.resetPasswordForm = this.fb.group({
+      email: ['',[Validators.required, Validators.email]]
+    })
+  }
+  onResetPassword() {
+    console.log(this.resetPasswordForm.value)
+  }
 }
