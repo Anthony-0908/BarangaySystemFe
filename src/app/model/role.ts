bcec9647e6f4fb6c283c815a5f18d31f81863e9e
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-15 00:30:50 +0800

    roles model

---
+++ src/app/model/role.ts
@@ -0,0 +1,10 @@
+import { Permission } from './permission';
+
+export interface Role {
+  id: number;
+  name: string;
+  guard_name: string;
+  created_at: string;
+  updated_at: string;
+  permissions: Permission[];
+}
