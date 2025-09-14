6ba2ea4521821148b145db6dcd0e92f56864cf38
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-15 00:31:02 +0800

    permission model

---
+++ src/app/model/permission.ts
@@ -0,0 +1,11 @@
+export interface Permission {
+  id: number;
+  name: string;
+  guard_name: string;
+  created_at: string;
+  updated_at: string;
+  pivot: {
+    role_id: number;
+    permission_id: number;
+  };
+}
