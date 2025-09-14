b19a9e65ad00d57d35512dbc5bbca59f9a75d1a4
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-15 00:32:01 +0800

    role store

---
+++ src/app/Admin/role/role.store.ts
@@ -0,0 +1,36 @@
+import {inject, Injectable} from "@angular/core";
+import { patchState, signalStore, withMethods, withState } from "@ngrx/signals";
+import { Role } from "../../model/role";
+import { RolesService } from "../../core/service/roles.service";
+import { firstValueFrom } from 'rxjs'; // <-- missing import
+
+interface RoleState{
+  roles: Role[];
+  loading: boolean;
+  error: string | null;
+
+}
+
+const initialState: RoleState = {
+  roles:[],
+  loading:false,
+  error:null,
+
+};
+
+@Injectable({ providedIn: 'root' })
+export class RolesStore extends signalStore(
+  withState(initialState),
+  withMethods((store, rolesService = inject(RolesService)) => ({
+    loadRoles: async (params?: any) => {
+      patchState(store, { loading: true, error: null });
+      try {
+        const roles = await firstValueFrom(rolesService.getRoles(params));
+        patchState(store, { roles:roles.data, loading: false });
+        console.log('Roles:', roles); 
+      } catch (err) {
+        patchState(store, { error: 'Failed to load roles', loading: false });
+      }
+    },
+  }))
+) {}
