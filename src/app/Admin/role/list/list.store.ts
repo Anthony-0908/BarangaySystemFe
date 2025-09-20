382cf91ef644c0be210af73f2d4c8477155f4123
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-20 20:15:31 +0800

    list store

---
+++ src/app/Admin/role/list/list.store.ts
@@ -0,0 +1,45 @@
+import { inject,Injectable } from "@angular/core";
+import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import { Role } from "../../../model/role";
+import { RolesService } from "../../../core/service/roles.service";
+import { firstValueFrom } from "rxjs";
+import { UserService } from "../../../core/service/user.service";
+
+interface RoleState { 
+  roles: Role[];
+  loading:boolean;
+  error:string | null;
+  selected: Role | null;
+}
+
+const initialState: RoleState = { 
+  roles: [],
+  loading:false,
+  error:null,
+  selected:null,
+}
+
+@Injectable({providedIn: 'root' })
+export class ListStore extends signalStore(
+  {providedIn: 'root'},
+  withState(initialState),
+  withMethods((store, roleService = inject(RolesService)) => ({
+    
+    loadRoles: async (params?: any): Promise<void> => { 
+      patchState(store, {loading:true , error:null});
+      try{
+        const res = await firstValueFrom(roleService.getRoles(params));
+        console.log('this is data' , res)
+        patchState(store, {roles: res, loading:false});
+      }catch(err)
+      {
+       patchState(store, {error:'Failed to load roles', loading:false});
+      }
+    },
+
+    selectedRole: (id:number) => { 
+      const role = store.roles().find(r => r.id === id) ?? null;
+      patchState(store, {selected:role});
+    },
+  }))
+){}
