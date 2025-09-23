484dc585bf14ae81ac8908cb8ddb4ad38a813ed4
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-23 22:51:32 +0800

    sidenavbar for the menu-items

--- src/app/layouts/sidenavbar/sidenavbar.component.ts
+++ src/app/layouts/sidenavbar/sidenavbar.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { MenuItem } from '../../menu-items.config';
+import { RouterModule } from '@angular/router';
+import { MENU_ITEMS } from '../../menu-items.config';
 @Component({
   selector: 'app-sidenavbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidenavbar.component.html',
   styleUrl: './sidenavbar.component.css'
 })
 export class SidenavbarComponent {
+    menuItems: MenuItem[] = [];
 
+  ngOnInit(): void {
+    // load menu items (could be filtered by role/permission later)
+    this.menuItems = MENU_ITEMS;
+  }
 }
