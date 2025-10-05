72100e4e69a6208b4f4cbfe1c14a5c9f94f67035
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-10-05 20:40:03 +0800

    fix the navbar and logout

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthStore } from '../../core/store/auth.store';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +10,8 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  private readonly store = inject(AuthStore)
+  onLogout() {
+    this.store.logout();
+  }
 }
