58986b5e1c383820c92e627caf070a3dc4ca4d2b
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-10-05 20:40:34 +0800

    user routes

--- src/app/Admin/user/user.route.ts
+++ src/app/Admin/user/user.route.ts
@@ -1,15 +1,26 @@
 import {Routes} from "@angular/router";
+import { authGuard } from "../../core/guards/auth.guard";
+
 export const USERS_ROUTES: Routes = [ 
   {
-    path:'',
-    loadComponent:() => import("./index/index.component").then(m => m.IndexComponent),
+    path: '',
+    loadComponent: () =>
+      import('./index/index.component').then(m => m.IndexComponent),
+    // canActivate: [authGuard],
+
   },
   {
-    path:'create',
-    loadComponent:() => import('./create/create.component').then(m => m.CreateComponent)
+    path: 'create',
+    loadComponent: () =>
+      import('./create/create.component').then(m => m.CreateComponent),
+    // canActivate: [authGuard],
+ 
   },
   {
-    path:':id/edit',
-    loadComponent:() => import('./update/update.component').then(m => m.UpdateComponent)
+    path: ':id/edit',
+    loadComponent: () =>
+      import('./update/update.component').then(m => m.UpdateComponent),
+    // canActivate: [authGuard],
+   
   }
 ]     
