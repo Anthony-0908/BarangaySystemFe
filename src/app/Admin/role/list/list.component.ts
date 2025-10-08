accc58787543a13705529aabf682db67e9e51a9f
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-10-08 21:46:36 +0800

    initial commit

--- src/app/Admin/role/list/list.component.ts
+++ src/app/Admin/role/list/list.component.ts
@@ -1,16 +1,27 @@
 import { Component, inject } from '@angular/core';
-import { ListStore } from './list.store';
-
+import { CommonModule } from '@angular/common';
+// import { ListStore } from './list.store';
+import { DataTableComponent } from "../../../shared/components/data-table/data-table.component";
+import { ColumnDef, DataTableParams,DataTableResponse} from '../../../shared/components/data-table/data-table.model';
+import { Router } from '@angular/router';
+import { Role } from '../../../model/role';
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [DataTableComponent, CommonModule],
   templateUrl: './list.component.html',
 
 })
 export class ListComponent {
-  private store = inject(ListStore)
+  // private router = inject(Router)
+  // private store = inject(ListStore)
 
-  ngOnInit(): void { 
-    this.store.loadRoles();
-  }
+  // columns:ColumnDef<Role>[] =[
+    
+  // ]
+  
+  // ngOnInit(): void { 
+  //   this.store.loadRoles();
+  // }
+
+
 }
