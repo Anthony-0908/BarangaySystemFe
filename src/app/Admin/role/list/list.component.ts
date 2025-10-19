1b0b0fc94d54259b824a351503f6fb54211030ce
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-10-19 12:38:40 +0800

    added columns and fetching roles

--- src/app/Admin/role/list/list.component.ts
+++ src/app/Admin/role/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { ListStore } from './list.store';
+import { ListStore } from './list.store';
 import { DataTableComponent } from "../../../shared/components/data-table/data-table.component";
 import { ColumnDef, DataTableParams,DataTableResponse} from '../../../shared/components/data-table/data-table.model';
 import { Router } from '@angular/router';
@@ -12,13 +12,33 @@ import { Role } from '../../../model/role';
 
 })
 export class ListComponent {
-  // private router = inject(Router)
-  // private store = inject(ListStore)
 
-  // columns:ColumnDef<Role>[] =[
-    
-  // ]
-  
+  private router = inject(Router)
+  private store = inject(ListStore)
+
+  columns: ColumnDef<Role>[] = [
+    { field: 'name', header: 'name', sortable: true, clickable: true },
+    { field: 'permissions', header: 'permissions', sortable: true },
+    { field: 'created_at', header: 'created at', sortable: true },
+    { field: 'updated_at' , header: 'updated at', sortable:true}
+  ];
+
+    fetchRoles = async (params:DataTableParams): Promise<DataTableResponse<Role>> => { 
+    await this.store.loadRoles(params)
+
+    return { 
+      data: this.store.roles(),
+      total: this.store.total(),
+    }
+  }
+
+  protected onEdit(role: Role) { 
+
+  }
+
+  protected onDelete(role: Role) { 
+
+  }
   // ngOnInit(): void { 
   //   this.store.loadRoles();
   // }
