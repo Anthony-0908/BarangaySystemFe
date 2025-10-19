65997fb562fdbfc0e0f1ef903c1c656462c65d13
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-10-19 12:35:35 +0800

    changed the the bootstraping to the appconfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,21 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([AuthInterceptor])),
     provideAnimationsAsync(),
-    provideHttpClient(
-      withInterceptors([authInterceptor])  // ✅ register here
-    ),
-    
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    })
+    // providePrimeNG({
+    //   theme: {
+    //     preset: Aura
+    //   }
+    // })
   ]
 };
